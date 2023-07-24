044db86dea0b2ab9359148203458fe7d9e8926b5
Mohamed Hassaan <mmhassaan3@gmail.com>
2023-07-24 22:24:35 +0400

    Responsive Screen

--- src/Api/PROPERTIES_API.js
+++ src/Api/PROPERTIES_API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "https://realtor.p.rapidapi.com/properties/"
+    baseURL: "https://realtor.p.rapidapi.com/properties/"
 })
